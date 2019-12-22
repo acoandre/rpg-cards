b4eae821a5a19eec014904eb2647ec25a76f3915
Michaela Spevacek <spemiki@gmail.com>
2019-12-22 12:44:30 +0100

    Add gradient option to default card settings

--- generator/js/ui.js
+++ generator/js/ui.js
@@ -389,6 +389,11 @@ function ui_change_default_icon_size() {
     ui_render_selected_card();
 }
 
+function ui_change_default_gradient() {
+    card_options.icon_gradient = $(this).is(':checked');
+    ui_render_selected_card();
+}
+
 function ui_sort() {
     $("#sort-modal").modal('show');
 }
@@ -538,6 +543,7 @@ $(document).ready(function () {
     $("#default-icon").change(ui_change_default_icon);
     $("#default-title-size").change(ui_change_default_title_size);
     $("#small-icons").change(ui_change_default_icon_size);
+    $("#gradient").change(ui_change_default_gradient);
 
     $(".icon-select-button").click(ui_select_icon);
 
